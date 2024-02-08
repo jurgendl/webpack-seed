fef37b35882e507d10797b1ae7bcb65f7c379cd1
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2024-02-08 14:18:02 +0100

    webpack seed

--- spec/app.spec.ts
+++ spec/app.spec.ts
@@ -1,14 +1,14 @@
-import { WebpackSeedApp } from "../src/components/webpack-seed-app";
+import {WebpackSeedApp} from "../src/components/webpack-seed-app";
 
 let app: WebpackSeedApp;
 
 describe('A suite is just a function', function () {
-  beforeEach(() => {
-    app = new WebpackSeedApp();
-  });
+	beforeEach(() => {
+		app = new WebpackSeedApp();
+	});
 
-  it('2 x 2 === 4', function () {
-    const res = app.init();
-	// expect(res).toBe(4);
-  });
+	it('2 x 2 === 4', function () {
+		const res = app.init();
+		expect(res).toBe("Webpack Seed App initialized!");
+	});
 });
--- src/components/webpack-seed-app.ts
+++ src/components/webpack-seed-app.ts
@@ -1,9 +1,10 @@
 import { MOD_CONST, ModClass } from "./webpack-seed-module";
 
 export class WebpackSeedApp {
-	init(): void {
+	init(): string {
 		console.log("Hello world!");
 		console.log(MOD_CONST);
 		console.log(new ModClass().modConst);
+		return "Webpack Seed App initialized!";
 	}
 }
