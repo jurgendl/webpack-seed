3ba4e2abf196691923ee3e902bba89d0a667e39a
jurgen de landsheer <jurgen.delandsheer@ugent.be>
2024-09-04 10:59:04 +0200

    webpack-seed

---
+++ httpserver-node.bat
@@ -0,0 +1,4 @@
+@rem https://www.npmjs.com/package/http-server
+call npm install --global http-server
+start http://localhost:12589/
+http-server --gzip --brotli --no-dotfiles --port 12589
---
+++ httpserver-python.bat
@@ -0,0 +1,3 @@
+@rem https://ryanblunden.com/create-a-http-server-with-one-command-thanks-to-python-29fcfdcd240e
+start http://localhost:12589/
+python -m http.server 12589
